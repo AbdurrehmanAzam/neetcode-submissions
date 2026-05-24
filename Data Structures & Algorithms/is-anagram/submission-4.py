c9963ff009ede4e44a2514e06e038bedbb700ae1
Muhammad Abdurrehman Azam <abdurrehmanazam300@gmail.com>
2026-05-24 20:19:53 +0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,7 @@
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            return collections.Counter(s) == collections.Counter(t)
+        else:
+            return 1 == 2
